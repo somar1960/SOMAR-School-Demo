9a7a37d5d0458ffdf366551aede36fdbad9889c9
somar1960 <somarhh93@gmail.com>
2026-07-14 00:06:46 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,23 @@
-import sqlite3
+import os
+import psycopg2
 
-DB_NAME = "students.db"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def connect():
-    return sqlite3.connect(DB_NAME)
+    return psycopg2.connect(DATABASE_URL)
 
 
 def create_table():
+
     conn = connect()
     cur = conn.cursor()
 
     cur.execute("""
         CREATE TABLE IF NOT EXISTS students(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            telegram_id INTEGER UNIQUE,
+            id SERIAL PRIMARY KEY,
+            telegram_id BIGINT UNIQUE,
             full_name TEXT,
             photo TEXT,
             student_number TEXT,
@@ -23,7 +26,9 @@ def create_table():
     """)
 
     conn.commit()
+    cur.close()
     conn.close()
+
 
 
 def add_student(telegram_id, full_name, photo):
@@ -32,11 +37,17 @@ def add_student(telegram_id, full_name, photo):
     cur = conn.cursor()
 
     cur.execute("""
-        INSERT OR REPLACE INTO students
+        INSERT INTO students
         (telegram_id, full_name, photo, status)
 
-        VALUES (?, ?, ?, ?)
-    """, (
+        VALUES (%s,%s,%s,%s)
+
+        ON CONFLICT (telegram_id)
+        DO UPDATE SET
+        full_name=EXCLUDED.full_name,
+        photo=EXCLUDED.photo
+    """,
+    (
         telegram_id,
         full_name,
         photo,
@@ -44,7 +55,10 @@ def add_student(telegram_id, full_name, photo):
     ))
 
     conn.commit()
+
+    cur.close()
     conn.close()
+
 
 
 def get_pending_students():
@@ -58,63 +72,9 @@ def get_pending_students():
         WHERE status='pending'
     """)
 
-    students = cur.fetchall()
+    data = cur.fetchall()
 
+    cur.close()
     conn.close()
 
-    return students
-
-
-def approve_student(student_id, student_number):
-
-    conn = connect()
-    cur = conn.cursor()
-
-    cur.execute("""
-        UPDATE students
-        SET status='approved',
-            student_number=?
-        WHERE id=?
-    """, (
-        student_number,
-        student_id
-    ))
-
-    conn.commit()
-    conn.close()
-
-
-def reject_student(student_id):
-
-    conn = connect()
-    cur = conn.cursor()
-
-    cur.execute("""
-        DELETE FROM students
-        WHERE id=?
-    """, (
-        student_id,
-    ))
-
-    conn.commit()
-    conn.close()
-
-
-def get_student(student_id):
-
-    conn = connect()
-    cur = conn.cursor()
-
-    cur.execute("""
-        SELECT *
-        FROM students
-        WHERE id=?
-    """, (
-        student_id,
-    ))
-
-    student = cur.fetchone()
-
-    conn.close()
-
-    return student
+    return data
